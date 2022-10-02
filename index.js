76f9ee24de7af39c5a8cc22c0e54137dc22d6a11
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-02 20:22:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let main = document.getElementById("main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Iman is the champion";
+document.body.append(newHeader);
